16c1ccdaab2161ba974bb0f566944da87c52054e
Rabiulrn <rnrabiul@gmail.com>
2021-09-13 07:58:22 +0600

    rating problem solved

--- js/app.js
+++ js/app.js
@@ -20,6 +20,8 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <h5>Rating count: ${product.rating.count}</h5>
+      <h6>Rating rate: ${product.rating.rate}</h6>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
